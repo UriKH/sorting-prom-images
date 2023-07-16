81a3538de068c65a208a8fce0564bca6a4217e0b
UriKH <urikh@icloud.com>
2023-07-16 03:08:14 +0300

    argparse

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
+import argparse
 from sort import Sorter
 
 
 def main():
-    sort = Sorter(r'', r'')
-    sort.sort(r'')
+    parser = argparse.ArgumentParser(description='Image sorter')
+    parser.add_argument('input_dir', help='the faces to search for are in this directory\'s images')
+    parser.add_argument('root_images', help='the images to search in - parent folder of the image folders')
+    args = parser.parse_args()
+
+    if not args.input_dir or not args.root_images:
+        raise Exception('Not all parameters given')
+
+    sort = Sorter(args.root_iamges, args.input_dir)
+    sort.sort()
 
 
 if __name__ == '__main__':
